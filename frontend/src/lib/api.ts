7aa30397c1f22acd2837bfb0d337a5e91d429a51
Kartikey Kushagra <149966127+Kartikey1405@users.noreply.github.com>
2025-12-14 22:55:51 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AnalysisResult } from '@/types/analysis'; // Ensure you have this type imported
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://gitgrade-c4nj.onrender.com';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
